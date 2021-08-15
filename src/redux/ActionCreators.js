30d2a298cb22a0fbc42c69b00ed554c41f68c12a
Scott Gerweck <scottgerweck@gmail.com>
2021-08-14 19:34:46 -0700

    workshop5 task2 working

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -183,3 +183,33 @@ export const addPartners = partners => ({
     type: ActionTypes.ADD_PARTNERS,
     payload: partners
 });
+
+export const postFeedback = (feedback) => dispatch => {
+    const newFeedback = { ...feedback };
+
+    return fetch(baseUrl + 'feedback', {
+            method: "POST",
+            body: JSON.stringify(newFeedback),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => { throw error; }
+        )
+        .then(response => response.json())
+        .then(console.log('Current state is: ' + JSON.stringify(newFeedback)))
+        .then(alert('Thank you for your feedback. \nYou submitted: ' + JSON.stringify(newFeedback)))
+        .catch(error => {
+            console.log('post feedback', error.message);
+            alert('Your feedback  could not be posted\nError: ' + error.message);
+        });
+};
