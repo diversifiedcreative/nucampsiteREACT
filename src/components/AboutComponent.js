21aafd0460997d57bcc664b7531c5075f4b46484
Scott Gerweck <scottgerweck@gmail.com>
2021-08-15 00:06:25 -0700

    week5 workshop complete | class project complete

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from 'react-animation-components';
 
 function RenderPartner({partner}) {
     if(partner) {
@@ -19,13 +20,14 @@ function RenderPartner({partner}) {
     return <div />;
 }
 
-
 function PartnerList(props) {
     const partners = props.partners.partners.map(partner => {
         return (
-            <Media tag="li" key="partner.id">
-                <RenderPartner partner={partner} />
-            </Media>
+            <Fade in key={partner.id}>
+                <Media tag="li">
+                    <RenderPartner partner={partner} />
+                </Media>
+            </Fade>
         );
     });
 
@@ -45,15 +47,17 @@ function PartnerList(props) {
             </div>
         )
     }
+
     return (
         <div className="col mt-4">
             <Media list>
-                {partners}
+                <Stagger in>
+                    {partners}
+                </Stagger>
             </Media>
         </div>
     );
 }
-
 
 function About(props) {
     return (
